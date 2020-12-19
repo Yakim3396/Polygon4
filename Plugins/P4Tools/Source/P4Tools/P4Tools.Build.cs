3ea55d50b58e86b4871bddbe1924be4b94037a0e
Yakim3396 <Watcover3396@gmail.com>
2020-12-19 05:20:36 +0300

    Del unneeded add public include path

--- Plugins/P4Tools/Source/P4Tools/P4Tools.Build.cs
+++ Plugins/P4Tools/Source/P4Tools/P4Tools.Build.cs
@@ -8,19 +8,6 @@ public class P4Tools : ModuleRules
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
 		
-		PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
-			
 		
 		PublicDependencyModuleNames.AddRange(
 			new string[]
